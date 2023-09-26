17e8a4ed4755dda6fcfb25aa7eb3fb5e17f926e2
yeeun <dpdmstjs@gmail.com>
2023-09-26 19:18:30 +0900

    feat : id 목록에 해당하는 snsBlock를 조회하는 findAllById 추가 (#30)

--- src/main/java/com/javajober/snsBlock/repository/SNSBlockRepository.java
+++ src/main/java/com/javajober/snsBlock/repository/SNSBlockRepository.java
@@ -1,9 +1,12 @@
 package com.javajober.snsBlock.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.Repository;
 
 import com.javajober.snsBlock.domain.SNSBlock;
 
 public interface SNSBlockRepository extends Repository<SNSBlock, Long> {
 	SNSBlock save(SNSBlock snsBlock);
+	List<SNSBlock> findAllById(List<Long> id);
 }
