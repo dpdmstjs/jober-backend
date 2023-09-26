7153b24a10892182f8b2ca0dd7b6c951b9792c05
yeeun <dpdmstjs@gmail.com>
2023-09-26 19:30:37 +0900

    feat : snsBlockIds 파라미터에 해당하는 모든 SNS 블록을 조회하는 find 메서드 구현 (#30)

--- src/main/java/com/javajober/snsBlock/service/SNSBlockService.java
+++ src/main/java/com/javajober/snsBlock/service/SNSBlockService.java
@@ -1,11 +1,16 @@
 package com.javajober.snsBlock.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.javajober.snsBlock.domain.SNSBlock;
 import com.javajober.snsBlock.dto.SNSBlockRequest;
 import com.javajober.snsBlock.dto.SNSBlockRequests;
+import com.javajober.snsBlock.dto.SNSBlockResponse;
+import com.javajober.snsBlock.dto.SNSBlockResponses;
 import com.javajober.snsBlock.repository.SNSBlockRepository;
 
 @Service
@@ -23,5 +28,15 @@ public class SNSBlockService {
 			SNSBlock snsBlock = SNSBlockRequest.toEntity(snsBlockrequest);
 			snsBlockRepository.save(snsBlock);
 		});
+	}
+
+	public SNSBlockResponses find(final List<Long> snsBlockIds) {
+
+		List<SNSBlockResponse> snsBlocks =
+			snsBlockRepository.findAllById(snsBlockIds).stream()
+				.map(SNSBlockResponse::from)
+				.collect(Collectors.toList());
+
+		return new SNSBlockResponses(snsBlocks);
 	}
 }
