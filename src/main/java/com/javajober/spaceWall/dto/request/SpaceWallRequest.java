5c1b0dbe975f3d6940d4e140567684814a2df17a
yeeun <dpdmstjs@gmail.com>
2023-10-01 18:36:24 +0900

    feat : SpaceWallRequest dto에 toEntity 메서드 생성 (#92)

--- src/main/java/com/javajober/spaceWall/dto/request/SpaceWallRequest.java
+++ src/main/java/com/javajober/spaceWall/dto/request/SpaceWallRequest.java
@@ -1,5 +1,11 @@
 package com.javajober.spaceWall.dto.request;
 
+import com.javajober.entity.AddSpace;
+import com.javajober.member.domain.Member;
+import com.javajober.spaceWall.domain.FlagType;
+import com.javajober.spaceWall.domain.SpaceWall;
+import com.javajober.spaceWall.domain.SpaceWallCategoryType;
+
 import lombok.Getter;
 
 @Getter
@@ -12,5 +18,16 @@ public class SpaceWallRequest {
 
     public SpaceWallRequest(final DataRequest data) {
         this.data = data;
+    }
+
+    public static SpaceWall toEntity(SpaceWallCategoryType category, Member member, AddSpace addSpace, String shareURL, FlagType flagType, String blocks) {
+        return SpaceWall.builder()
+            .spaceWallCategoryType(category)
+            .member(member)
+            .addSpace(addSpace)
+            .shareURL(shareURL)
+            .flag(flagType)
+            .blocks(blocks)
+            .build();
     }
 }
