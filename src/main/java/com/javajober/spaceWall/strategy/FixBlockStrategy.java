a5921112327511e46def6d4fd424896938e8de0d
yeeun <dpdmstjs@gmail.com>
2023-10-22 07:04:36 +0900

    refactor : FixBlockStrategy 인터페이스에 saveBlocks와 getStrategyName 메서드 정의 (#179)

--- src/main/java/com/javajober/spaceWall/strategy/FixBlockStrategy.java
+++ src/main/java/com/javajober/spaceWall/strategy/FixBlockStrategy.java
@@ -1,5 +1,9 @@
 package com.javajober.spaceWall.strategy;
 
-public interface FixBlockStrategy {
+import com.javajober.spaceWall.dto.request.DataStringSaveRequest;
 
+public interface FixBlockStrategy {
+	Long saveBlocks(final DataStringSaveRequest data);
+
+	String getStrategyName();
 }
