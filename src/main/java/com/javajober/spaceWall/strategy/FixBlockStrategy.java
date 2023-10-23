d14eec70863ab9399ed05150742c2ffbc78c36a7
yeeun <dpdmstjs@gmail.com>
2023-10-23 20:15:12 +0900

    refactor : FixBlockStrategy 인터페이스에 saveBlocks 메서드 매개변수 수정 (#179)

--- src/main/java/com/javajober/spaceWall/strategy/FixBlockStrategy.java
+++ src/main/java/com/javajober/spaceWall/strategy/FixBlockStrategy.java
@@ -1,13 +1,14 @@
 package com.javajober.spaceWall.strategy;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.javajober.core.util.response.CommonResponse;
 import com.javajober.spaceWall.dto.request.DataStringSaveRequest;
 
 import java.util.List;
 
 public interface FixBlockStrategy {
-	Long saveBlocks(final DataStringSaveRequest data);
+	void saveBlocks(final DataStringSaveRequest data, ArrayNode blockInfoArray, Long position);
 
 	CommonResponse createFixBlockDTO(List<JsonNode> fixBlocks);
 
