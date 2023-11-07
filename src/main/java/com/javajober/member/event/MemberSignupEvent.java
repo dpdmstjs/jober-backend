f32e4881e03ae77fdc8f4320e827184af236bf2b
yeeun <dpdmstjs@gmail.com>
2023-11-07 21:58:47 +0900

    refactor : MemberSignupEvent 클래스 필드 제거 (#228)

--- src/main/java/com/javajober/member/event/MemberSignupEvent.java
+++ src/main/java/com/javajober/member/event/MemberSignupEvent.java
@@ -1,7 +1,6 @@
 package com.javajober.member.event;
 
 import com.javajober.member.domain.Member;
-import com.javajober.space.dto.request.SpaceSaveRequest;
 
 import lombok.Getter;
 
@@ -9,15 +8,8 @@ import lombok.Getter;
 public class MemberSignupEvent {
 
 	private final Member member;
-	private final SpaceSaveRequest personalSpaceRequest;
-	private final SpaceSaveRequest organizationSpaceRequest;
 
-	public MemberSignupEvent(
-		final Member member, final SpaceSaveRequest personalSpaceRequest,
-		final SpaceSaveRequest organizationSpaceRequest
-	) {
+	public MemberSignupEvent(final Member member) {
 		this.member = member;
-		this.personalSpaceRequest = personalSpaceRequest;
-		this.organizationSpaceRequest = organizationSpaceRequest;
 	}
 }
